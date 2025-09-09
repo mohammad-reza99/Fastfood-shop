7e447b42e17d66182620ffdebfba4ce6042d23ba
Mohammad Reza Zaeri <rezazaeri1990@gmail.com>
2025-09-09 21:27:19 +0200

    Add Hero component and image; integrate into App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import React from "react";
 import Navbar from "./assets/components/Navbar";
+import Hero from "./assets/components/Hero";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   );
 }
---
+++ src/assets/components/Hero.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import bImg from "../image/pexels-daniela-elena-tentis-118658-750073.jpg";
+const Hero = () => {
+  return (
+    <div className="max-w-[1640px] mx-auto p-4">
+      <div className="max-h-[700px] relative">
+        <div className="absolute w-full h-full text-gray-200 max-h-[700px] bg-black/40 flex flex-col justify-center text-left">
+          <h1 className=" px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold">
+            The <span className="text-orange-500">Best</span>
+          </h1>
+          <h1 className=" px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold">
+            Foods <span className="text-orange-500">Delivered</span>
+          </h1>
+        </div>
+        <img src={bImg} alt="/" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
